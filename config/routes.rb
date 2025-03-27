ea1bc2c5ac89201fe07bd779b4b9f60d0f23d73d
Josefadarrouymarin <jidarrouy@miuandes.cl>
2025-03-26 23:39:02 -0300

    arreglando

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
+  get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
-  post "/contact", to: "pages#contact"
+  post "/contact", to: "pages#contact"  
 end
