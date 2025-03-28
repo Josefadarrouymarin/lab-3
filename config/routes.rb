3e8288cb67f90718a7e0863c598ed3799f2cb7c3
Josefadarrouymarin <jidarrouy@miuandes.cl>
2025-03-27 23:05:12 -0300

    de

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
-  get 'pages/contact', to: 'pages#contact'
+  root "pages#home"
+  get "/home", to: "pages#home", as: "home"
+  get "/about", to: "pages#about", as: "about"
+  get "/contact", to: "pages#contact", as: "contact"
+  post "/contact", to: "pages#contact"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,7 +14,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
